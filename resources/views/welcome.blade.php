292f9d199c3a402b8a81e330dc0e0da45eaee83a
ken.takahashi <iinicoffee7112@gmail.com>
2021-11-30 02:23:35 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -83,6 +83,18 @@
                 <div class="title m-b-md">
                     Tasklist
                 </div>
+                
+                @extends('layouts.app')
+
+                @section('content')
+                    <div class="center jumbotron">
+                    <div class="text-center">
+                        <h1>Welcome to the Microposts</h1>
+                        {{-- ユーザ登録ページへのリンク --}}
+                        {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                    </div>
+                </div>
+                @endsection
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
