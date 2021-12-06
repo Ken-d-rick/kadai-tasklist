bbc285f2259d236b752c3f470f0ce5b882db6f85
ken.takahashi <iinicoffee7112@gmail.com>
2021-12-06 07:30:45 +0000

    post

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -17,9 +17,7 @@ class TasksController extends Controller
             $user = \Auth::user();
             
             $tasks = $user->tasks()->orderBy('created_at', 'desc')->paginate(10);
-            
-            // タスク一覧を取得
-            $tasks = Task::all();
+
             // タスク一覧ビューでそれを表示
             return view('tasks.index', [
                 'tasks' => $tasks,
@@ -74,6 +72,12 @@ class TasksController extends Controller
     {
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
+        
+        // 認証済みユーザ（閲覧者）がその投稿の所有者でない場合はトップページにリダイレクト
+        if (\Auth::id() != $task->user_id) {
+            // トップページへリダイレクトさせる
+            return redirect('/');
+        }
 
         
         // タスク詳細ビューでそれを表示
@@ -86,6 +90,13 @@ class TasksController extends Controller
     {
         // idの値でタスクを検索して取得
         $task = Task::findOrFail($id);
+        
+        // 認証済みユーザ（閲覧者）がその投稿の所有者でない場合はトップページにリダイレクト
+        if (\Auth::id() != $task->user_id) {
+            // トップページへリダイレクトさせる
+            return redirect('/');
+        }
+
 
         // タスク編集ビューでそれを表示
         return view('tasks.edit', [
@@ -115,7 +126,7 @@ class TasksController extends Controller
             //タスクを編集
             $task->status = $request->status;    // 追加
             $task->content = $request->content;
-            $task->save();
+            $task->save(); 
         }
 
         // トップページへリダイレクトさせる
--- resources/views/commons/navbar.blade.php
+++ resources/views/commons/navbar.blade.php
@@ -11,15 +11,16 @@
             <ul class="navbar-nav">
                 @if (Auth::check())
                     {{-- ユーザ一覧ページへのリンク --}}
-                    <li class="nav-item"><a href="#" class="nav-link"></a></li>
                     <li class="nav-item dropdown">
                         <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown">{{ Auth::user()->name }}</a>
                         <ul class="dropdown-menu dropdown-menu-right">
                             {{-- ユーザ詳細ページへのリンク --}}
                             <li class="dropdown-item"><a href="#">My profile</a></li>
+                            {{-- ユーザ登録ページへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('tasks.create', 'タスク新規作成', []) !!}</li>
                             <li class="dropdown-divider"></li>
-                　　　　　　　　<li class="nav-item">{!! link_to_route('tasks.create', '新規タスクの投稿', [], ['class' => 'nav-link']) !!}</li>
-                            <li class="dropdown-item">{!! link_to_route('logout.get','Logout') !!}</li>
+                            {{-- ログアウトへのリンク --}}
+                            <li class="dropdown-item">{!! link_to_route('logout.get', 'Logout') !!}</li>
                         </ul>
                     </li>
                 @else
@@ -28,7 +29,6 @@
                     {{-- ログインページへのリンク --}}
                     <li class="nav-item">{!! link_to_route('login', 'Login', [], ['class' => 'nav-link']) !!}</li>
                 @endif
-                
             </ul>
         </div>
     </nav>
